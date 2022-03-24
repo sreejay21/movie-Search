99938081037a1b3ec689617fd46690c8cd58eaa4
sreejay21 <79089737+sreejay21@users.noreply.github.com>
2022-03-24 12:27:22 +0530

    Update movieSearch.js

--- movieSearch.js
+++ movieSearch.js
@@ -1,37 +1,81 @@
 
-var movieSearchElement = document.getElementById("input-tag")
-var button = document.getElementById("search-btn")
-var movieCardWrapper=document.getElementById("Card-Wrapper")
+var inputElement = document.getElementById("movie-name")
 
-button.addEventListener("click", function () {
-    var xhttp = new XMLHttpRequest()
-    movieName = movieSearchElement.value
-    api = "https://www.omdbapi.com/?apikey=c951ff1&s=" + movieName
-    xhttp.onreadystatechange = function () {
-        if (this.readyState == 4) {
-            var result = JSON.parse(this.responseText)
-            console.log(result)
-            if(result.Response == "True"){
-               var movies=result.Search;
-               
-               for(var i=0;i<movies.length;i++){
-                   var moviecard=document.createElement("div")
-                   moviecard.className="card"
-                   var poster=document.createElement("img")
-                   poster.classname="movie-image"
-                   poster.src=movies[i].Poster
-                   moviecard.appendChild(poster)
-                   movieCardWrapper.appendChild(moviecard)
-                   
-               }
+var searchButton = document.getElementById("search-btn");
 
-            }
-            else{
-                movieCardWrapper.innerText="404 movie not found"
-            }
-        }
-    }
-   
-    xhttp.open("GET", api, true)
-    xhttp.send()
-})
+var moviesWrapper = document.getElementById("movies-wrapper")
+
+  var resultDiv =  document.getElementById("result-div")
+
+  var loaderDiv = document.getElementById("loader")
+
+searchButton.onclick = function(){
+
+    if(inputElement.value !== "" ){
+ 
+    resultDiv.innerHTML = ""
+    
+    moviesWrapper.innerHTML = ""
+
+    loaderDiv.innerText = "Loading......."
+
+
+    
+ var http = new XMLHttpRequest();
+
+ http.onreadystatechange = function(){
+
+      if(this.readyState == 4){
+
+        inputElement.value = ""
+
+        loaderDiv.innerText = ""
+    
+       
+      var result =  JSON.parse(this.responseText)
+
+      console.log(result)
+      
+      if(result.Response == "True"  ){
+        var movies = result.Search
+        resultDiv.innerText = "Total results : "+ movies.length
+         for( var i = 0 ; i < movies.length ; i++ ){
+             // POSTER , TITLE , RELEASE YEAR , TYPE
+             var movieCard = document.createElement("div");
+             movieCard.className = "movie-card"
+             var movieImage =  document.createElement("img");
+             movieImage.src = movies[i].Poster
+             movieImage.className = "movie-image"
+             var movieTitle = document.createElement("p");
+             movieTitle.innerText = "Title : " +movies[i].Title
+             var Type = document.createElement("p");
+             Type.innerText = "Type : " + movies[i].Type
+             var releaseYear = document.createElement("p")
+             releaseYear.innerText = "Release Year : " + movies[i].Year
+             movieCard.append(movieImage, movieTitle,Type,releaseYear )
+             moviesWrapper.appendChild(movieCard)
+            
+
+         }
+      }else{
+        resultDiv.innerHTML = `404 Movies not found !`
+          console.log("Movies not found")
+      }
+
+      
+      }
+
+ }
+
+ http.open("GET" , "https://www.omdbapi.com/?apikey=c951ff1&s="+ inputElement.value , true);
+
+http.send()
+
+}else{
+    alert("Type something")
+}
+}
+
+
+
+
